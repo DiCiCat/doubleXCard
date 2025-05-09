7570fe765a2d3955b83a18cc4eec0bc1ed20b350
DiCiCat <dicicat@mail.fr>
2025-05-09 14:01:42 +0200

    Add files via upload

--- src/XCard.js
+++ src/XCard.js
@@ -1,84 +1,86 @@
 class XCard extends Application {
-
-    constructor() {
+    constructor(imagePath) {
         super();
+        this.imagePath = imagePath;
     }
 
     static get defaultOptions() {
-        const options = super.defaultOptions; 
+        const options = super.defaultOptions;
         options.template = "modules/XCard/templates/XCard.html";
         options.title = "XCard.WindowTitle";
         options.id = "XCard";
         options.resizable = false;
-        if (game.settings.get("XCard","imageToggle")){
-            options.height = game.settings.get("XCard","imageSize").height+75;
-            options.width = game.settings.get("XCard","imageSize").width+50;
+        if (game.settings.get("XCard", "imageToggle")) {
+            options.height = game.settings.get("XCard", "imageSize").height + 75;
+            options.width = game.settings.get("XCard", "imageSize").width + 50;
         } else {
-            options.height="auto";
-            options.width="auto";
+            options.height = "auto";
+            options.width = "auto";
         }
         return options;
     }
 
     async getData() {
         const imageToggle = game.settings.get("XCard", "imageToggle");
-        const imagePath = game.settings.get("XCard", "imagePath");
 
         let imageWidth = 0;
         let imageHeight = 0;
         if (imageToggle) {
-            const tex = await loadTexture(imagePath);
+            const tex = await loadTexture(this.imagePath);
             imageWidth = tex.width;
             imageHeight = tex.height;
         }
 
         return {
             imageToggle: imageToggle,
-            imagePath: imagePath,
+            imagePath: this.imagePath,
             imageWidth: imageWidth,
             imageHeight: imageHeight
         }
     }
 }
 
-Hooks.on('getSceneControlButtons', function(hudButtons)
-{
-    if (foundry.utils.isNewerVersion(game.version, 13)){
-        hudButtons.tokens.tools.xCard = {
-            name: "xCard",
-            title: "XCard.ButtonHint",
-            icon: game.i18n.localize("XCard.ButtonFAIcon"),
-            onChange: async (event, active) => {
-              if ( active ) {
-                let xc = new XCard();
+Hooks.on('getSceneControlButtons', function (hudButtons) {
+    const xCardIcon = game.i18n.localize("XCard.ButtonFAIcon");
+    const xCardIcon2 = game.i18n.localize("XCard.ButtonFAIcon2");
+    const group = hudButtons.find(b => b.name === "token");
+    if (!group) return;
+
+    group.tools.push(
+        {
+            name: "XCard1",
+            title: "Orange",
+            icon: xCardIcon,
+            button: true,
+            onClick: async () => {
+                const path = game.settings.get("XCard", "imagePath");
+                let xc = new XCard(path);
                 xc.render(true);
-                game.socket.emit("module.XCard", {"event": "XCard"})
-              }
-            },
-            button: true
-          };
-    } else {
-        let hud = hudButtons.find(val => { return val.name == "token"; })
-        if (hud) {
-            hud.tools.push({
-                name: "XCard.ButtonName",
-                title: "XCard.ButtonHint",
-                icon: game.i18n.localize("XCard.ButtonFAIcon"),
-                button: true,
-                onClick: async () => {
-                    let xc = new XCard();
-                    await xc.render(true);
-                    game.socket.emit("module.XCard", {"event": "XCard"})
-                }
-            });
+                game.socket.emit("module.XCard", { event: "XCard1" });
+            }
+        },
+        {
+            name: "XCard2",
+            title: "Red",
+            icon: xCardIcon2,
+            button: true,
+            onClick: async () => {
+                const path = game.settings.get("XCard", "imagePath2");
+                let xc = new XCard(path);
+                xc.render(true);
+                game.socket.emit("module.XCard", { event: "XCard2" });
+            }
         }
-    }
+    );
 });
 
 Hooks.once('ready', async function () {
-
     game.socket.on("module.XCard", data => {
-        let xc = new XCard();
+        let path = (data.event === "XCard1") ?
+            game.settings.get("XCard", "imagePath") :
+            game.settings.get("XCard", "imagePath2");
+
+        let xc = new XCard(path);
         xc.render(true);
     });
 
@@ -93,30 +95,39 @@ Hooks.once('ready', async function () {
 
     game.settings.register("XCard", "imageSize", {
         config: false,
-        type:Object,
-        default:{"width":300,"height":500}
-    })
+        type: Object,
+        default: { "width": 300, "height": 500 }
+    });
 
     game.settings.register("XCard", "imagePath", {
-        name: "XCard.Settings.ImagePathName",
-        hint: "XCard.Settings.ImagePathHint",
+        name: "XCard.Settings.ImagePathName1",
+        hint: "XCard.Settings.ImagePathHint1",
         scope: "world",
         config: true,
         type: String,
-        default:"",
+        default: "",
         onChange: async value => {
             const tex = await loadTexture(value);
-            imageWidth = tex.width;
-            imageHeight = tex.height;
-            await game.settings.set("XCard", "imageSize",{"width":imageWidth,"height":imageHeight});
+            await game.settings.set("XCard", "imageSize", { width: tex.width, height: tex.height });
         }
     });
 
-    if (game.settings.get("XCard","imageToggle")){
-        const tex = await loadTexture(game.settings.get("XCard","imagePath"));
-            imageWidth = tex.width;
-            imageHeight = tex.height;
-            await game.settings.set("XCard", "imageSize",{"width":imageWidth,"height":imageHeight});
-    }
+    game.settings.register("XCard", "imagePath2", {
+        name: "XCard.Settings.ImagePathName2",
+        hint: "XCard.Settings.ImagePathHint2",
+        scope: "world",
+        config: true,
+        type: String,
+        default: "",
+        onChange: async value => {
+            const tex = await loadTexture(value);
+            await game.settings.set("XCard", "imageSize", { width: tex.width, height: tex.height });
+        }
+    });
 
+    if (game.settings.get("XCard", "imageToggle")) {
+        const tex = await loadTexture(game.settings.get("XCard", "imagePath"));
+        await game.settings.set("XCard", "imageSize", { width: tex.width, height: tex.height });
+    }
 });
+
